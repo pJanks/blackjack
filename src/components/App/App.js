20d78c37806cfa1c3e7adf780526e5f21e1dd556
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-05-12 08:50:51 -0700

    Refactor file structure to fix buggy behavior

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,43 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PlayerHand from '../PlayerHand/PlayerHand'
 import DealerHand from '../DealerHand/DealerHand'
+import Game from '../Game/Game'
 import './App.css';
 
-const suits = ['D', 'S', 'H', 'C']
-const types = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
-const deck = []
-let counter = 2
-
-
-
-suits.forEach(suit => {
-  types.forEach(type => {
-    deck.push(`${suit}${type}`)
-  })
-})
-
-for (let i = deck.length - 1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i + 1));
-  [deck[i], deck[j]] = [deck[j], deck[i]];
-}
 
 const App = () => {
-  const [dealerHand, setDealerHand] = useState([deck[0], deck[1]])
-  const [playerHand, setPlayerHand] = useState([deck[11], deck[12]])
-  const handleHitButtonClick = (e) => {
-    setPlayerHand([...playerHand, deck[counter]])
-    counter ++
-  }
+
+
   return (
     <main className="game-board">
-      <DealerHand cards={ dealerHand } />
-      <section>
-        <button
-        onClick={ (e) => handleHitButtonClick(e) }
-        >Hit</button>
-        <button>Stand</button>
-      </section>
-      <PlayerHand cards={ playerHand } />
+      <Game />
     </main>
   );
 }
